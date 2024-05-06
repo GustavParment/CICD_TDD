9b62cc21c9bcab7b947ef6217c9447b89c1bbc7f
GustavParment <gustav.parment1993@gmail.com>
2024-05-06 11:12:09 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public String toNameFormat (String name) {
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+}
